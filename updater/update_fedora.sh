d95a11076096b56b390478993fe5f388de606e3f
Ujjwal Pandey <ujpandey1@cougars.ccis.edu>
2015-07-25 18:59:53 -0500

    Add some minor cleanup.

--- updater/update_fedora.sh
+++ updater/update_fedora.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+pushd ~
+mkdir tmp-fedora-update
+pushd tmp-fedora-update
 wget -d https://raw.githubusercontent.com/cccslab/fedora-utils/master/updater/updater.py
+wget -d https://raw.githubusercontent.com/cccslab/fedora-utils/master/updater/client_secrets.json
 python updater.py
+popd
+rm -rf tmp-fedora-update
+popd
+
