b91c7b69c371a5f3036b3c64ef1413d89eef08f4
Ujjwal Pandey <ujpandey1@cougars.ccis.edu>
2015-07-24 21:35:48 -0500

    Fix links in distributed script and make it executable.

--- updater/update_fedora.sh
+++ updater/update_fedora.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -d https://raw.githubusercontent.com/cccslab/fedora-utils/master/updater.py
+wget -d https://raw.githubusercontent.com/cccslab/fedora-utils/master/updater/updater.py
 python updater.py
